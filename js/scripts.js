0bd3fe86610818880616973c2379e691a7a785c4
abisoye <beesoye111@gmail.com>
2022-04-30 20:41:22 +0100

    using input and label

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    let ballot = parsInt(prompt("how many numbers are there"));
+
+    if (ballot === 30) {
+
+        alert("amazing!!!")
+    }
+
+    elseif(ballot > 30); {
+
+
+        alert("the answer is greater" + ballot)
+
+    }
+
+
+elseif(ballot < 30); {
+
+alert("the answer is not less than" + ballot)
+
+
+}
+
+elseif(ballot <= 30); {
+
+    alert("the answer is not less than" = ballot)
+}
+
+
+
+
+
+
+
+
+
+
+
+});
