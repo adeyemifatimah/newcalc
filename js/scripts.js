52ff14e81c054256a15193f9ae5aeaacb1523bf8
abisoye <beesoye111@gmail.com>
2022-04-30 21:32:12 +0100

    creating a business interphase

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,10 @@
 $(document).ready(function () {
-    let ballot = parsInt(prompt("how many numbers are there"));
 
-    if (ballot === 30) {
-
-        alert("amazing!!!")
-    }
-
-    elseif(ballot > 30); {
+ function add(number1,number2) {
 
 
-        alert("the answer is greater" + ballot)
-
-    }
-
-
-elseif(ballot < 30); {
-
-alert("the answer is not less than" + ballot)
-
-
-}
-
-elseif(ballot <= 30); {
-
-    alert("the answer is not less than" = ballot)
-}
+    
+ }
 
 
 
