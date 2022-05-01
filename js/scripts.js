1cc3a7c8b064ba1f45e417bd75efbf668d764f69
abisoye <beesoye111@gmail.com>
2022-05-01 07:03:29 +0100

    giving valur to input

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,55 @@
+
+function add(number1, number2) {
+    return (number1 + number2);
+
+
+}
+
+function subtraction(number1, number2) {
+    return (number1 - number2);
+
+
+}
+
+function division(number1, number2) {
+    return (number1 / number2);
+
+
+}
+
+function multiplication(number1, number2) {
+    return (number1 * number2);
+
+
+}
+
+function modulo(number1, number2) {
+    return (number1 % number2);
+
+
+}
+
+
+
 $(document).ready(function () {
-
- function add(number1,number2) {
-
-
-    
- }
+    $("#calculator").submit(function (event) {
+        event.preventDefault()
 
 
+        const number1 = parseInt$("#num1").val();
+        const number2 = parseInt$("#num2").val();
 
 
 
-
+const
 
 
 
 
 
+
+
+
+
+    });
 });
