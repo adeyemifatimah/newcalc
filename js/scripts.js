1824fef1463e516aa0ae3b4bb4c8d9ffad51dca9
abisoye <beesoye111@gmail.com>
2022-05-03 07:43:48 +0100

    updated my assigment operator to comparison operator

--- js/scripts.js
+++ js/scripts.js
@@ -31,31 +31,35 @@ function modulo(number1, number2) {
 
 
 
+
+
+
 $(document).ready(function () {
     $("#calculator").submit(function (event) {
         event.preventDefault()
 
 
-        const number1 = parseInt($("#num1").val());
-        const number2 = parseInt($("#num2").val());
+        const number1 = parseInt($("input#number1").val());
+        const number2 = parseInt($("input#number2").val());
 
 
 
         const operator = $("input:radio[name=operator]:checked").val();
+let result;
 
 
-        if (operator = addition) {
+        if (operator === addition) {
 
             let result = add(number1, number2);
         }
 
 
-        else if(operator = subtraction) {
+        else if (operator === subtraction) {
 
             let result = subtract(number1, number2);
         }
 
-        else if(operator = division) {
+        else if (operator ===division) {
 
             let result = divide(number1, number2);
 
@@ -64,19 +68,21 @@ $(document).ready(function () {
 
 
 
-        else if(operator = multiplication) {
+        else if (operator === multiplication) {
 
             let result = multiply(number1, number2);
 
         }
 
 
-        
-        else if(operator = modulo) {
 
-            let result = module(number1, number2);
+        else if (operator === modulo) {
+
+            let result = modulo(number1, number2);
 
         }
+
+      return  $("#output").text(result);
 
 
 
